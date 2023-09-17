f4e869f34416bb93bd6a0475b12666554cacab94
abhidharshss <abhidharsh1999@gmail.com>
2023-09-17 15:38:54 +0530

    4

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('homepage/',views.homepage,name='homepage'),
     path('listposts/',views.listposts,name='listposts'),
-    path('postdetail/<int:postindex>/',views.postdetail,name='postdetail'),
+    path('postdetail/<int:postid>/',views.postdetail,name='postdetail'),
+    path('Updatepost/<int:postid>/',views.Updatepost,name='Updatepost'),
+    path('Deletepost/<int:postid>/',views.Deletepost,name='Deletepost'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,24 +2,27 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from .models import Post
+from .serializer import PostSerializer
+from django.shortcuts import get_object_or_404
 
 
 # def homepage(request:HttpRequest):
 #     response = {'message':'Hello world'}
 #     return JsonResponse(data=response)
 
-posts=[
-    {
-        "id":1,
-        'title':'is programming difficult ?',
-        'description':'Programming is simple because it needs only logic to build something.'
-    },
-    {
-        'id':2,
-        'title':'Learn Python',
-        'description':'Python is a high level programming language.'
-    }
-]
+# posts=[
+#     {
+#         "id":1,
+#         'title':'is programming difficult ?',
+#         'description':'Programming is simple because it needs only logic to build something.'
+#     },
+#     {
+#         'id':2,
+#         'title':'Learn Python',
+#         'description':'Python is a high level programming language.'
+#     }
+# ]
 
 @api_view(http_method_names=['GET','POST'])
 def homepage(request:Request):
@@ -32,14 +35,56 @@ def homepage(request:Request):
     response = {'message':'Hello world, GET'}
     return Response(data=response,status=status.HTTP_200_OK)
 
-@api_view(http_method_names=['GET'])
+@api_view(http_method_names=['GET','POST'])
 def listposts(request:Request):
-    return Response(data=posts,status=status.HTTP_200_OK)
+    posts=Post.objects.all()
+    if request.method == 'POST':
+        data=request.data
+        serializer=PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            response={
+                'message':'Post created successfully',
+                'data':serializer.data
+            }
+            return Response(data=response,status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    serializer=PostSerializer(instance=posts,many=True)
+    response={
+        'message':'All posts',
+        'data':serializer.data
+    }
+    return Response(data=response,status=status.HTTP_200_OK)
 
 @api_view(http_method_names=['GET'])
-def postdetail(request:Request,postindex:int):
-    try:
-        data=posts[postindex-1]
-        return Response(data=data,status=status.HTTP_200_OK)
-    except:
-        return Response(data={'error':'No data found'},status=status.HTTP_404_NOT_FOUND)
+def postdetail(request:Request,postid:int):
+        data=get_object_or_404(Post,id=postid)
+        serialzer=PostSerializer(instance=data)
+        response={
+             'message':'Post detail',
+             'data':serialzer.data
+        }
+        return Response(data=response,status=status.HTTP_200_OK)
+
+@api_view(http_method_names=['PUT'])
+def Updatepost(reqeust:Request,postid:int):
+     post=get_object_or_404(Post,id=postid)
+     data=reqeust.data
+     serializer=PostSerializer(instance=post,data=data)
+     if serializer.is_valid():
+          serializer.save()
+          response={
+               'message':'Post updated successfully',
+               'data':serializer.data
+          }
+          return Response(data=response,status=status.HTTP_302_FOUND)
+     return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(http_method_names=['DELETE'])
+def Deletepost(request:Request,postid:int):
+     post=get_object_or_404(Post,id=postid)
+     post.delete()
+     response={
+          'message':'Post deleted successfully'
+     }
+     return Response(data=response,status=status.HTTP_200_OK)
