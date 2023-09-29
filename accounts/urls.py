e74da3f92cfef7958ced3a43aaa17b505f243b79
abhidharshss <abhidharsh1999@gmail.com>
2023-09-29 16:32:06 +0530

    token authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('SignUpView/',views.SignUpView.as_view(),name='SignUpView'),
+    path('LoginView/',views.LoginView.as_view(),name='LoginView'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,14 @@ from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 class SignUpView(GenericAPIView):
+    permission_classes=(AllowAny,)
     serializer_class=SignUpSerializer
     
     def post(self,request:Request):
@@ -21,3 +25,25 @@ class SignUpView(GenericAPIView):
             }
             return Response(data=response,status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    permission_classes=(AllowAny,)
+    def post(self,request:Request):
+        email=request.data.get('email')
+        password=request.data.get('password')
+        user=authenticate(email=email,password=password)
+        if user is not None:
+            response={
+                'message':'Login Successfull',
+                'token':user.auth_token.key
+            }
+            return Response(data=response,status=status.HTTP_200_OK)
+        else:
+            return Response(data={'Invalid Username or Password'},status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self,request:Request):
+        content={
+            'user':str(request.user),
+            'token':str(request.auth)    
+        }
+        return Response(data=content,status=status.HTTP_200_OK)
